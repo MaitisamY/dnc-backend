b646ebca9b2c314175d0db0b50c95bd0f3f74d8c
Muhammad Aitisam Yaseen <145204832+MaitisamY@users.noreply.github.com>
2024-05-13 11:22:31 -0700

    re update

--- app.js
+++ app.js
@@ -23,13 +23,9 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const corsOptions = {
-    origin: 'https://dnc-frontend.vercel.app/', 
-    optionsSuccessStatus: 200 
-}
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 app.use(session({
